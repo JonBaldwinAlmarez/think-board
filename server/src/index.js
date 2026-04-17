a530f0e05c8b710f7f90f8023d61d8707c483b78
Jon Baldwin Almarez <88573999+JonBaldwinAlmarez@users.noreply.github.com>
2026-04-17 19:30:13 +0800

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -8,5 +8,3 @@ app.use("/api/notes", notesroutes);
 app.listen(5000, () => {
 	console.log("Server is running on port 5000");
 });
-
-// mongodb+srv://almarezjbm_db_user:tNLNh37b1qhIDunW@cluster0.vp1hno7.mongodb.net/?appName=Cluster0
