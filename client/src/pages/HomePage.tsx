6a68e4ea2a866f0d7b7f5f99316b275a963fbd7a
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-04-27 19:17:09 +0800

    Typo: Fix import NavBar typo

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import NavBar from "../components/navbar";
+import NavBar from "../components/NavBar";
 import { useEffect, useState } from "react";
 import RateLimitedUI from "../components/RateLimitedUI";
 import toast from "react-hot-toast";
