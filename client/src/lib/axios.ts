08d93ffdb69eb9997ca28e1ae5dd1fe3140f9f97
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-04-27 19:44:50 +0800

    Fix API base URL for Render deployment

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-// in production no localhost so we have to make this dynamic
 const BASE_URL =
-	import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+	import.meta.env.MODE === "development"
+		? "http://localhost:5000/api"
+		: import.meta.env.VITE_API_URL;
 
 const api = axios.create({
 	baseURL: BASE_URL,
