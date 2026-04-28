8786f0ef75496178f1e541d30ddc45c08c0ef18e
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-04-28 14:34:29 +0800

    Fix: backend axios typo

--- server/src/index.js
+++ server/src/index.js
@@ -26,9 +26,9 @@ app.use(express.json());
 app.use(rateLimit);
 
 app.use("/api/notes", noteRoutes);
-app.use(express.static(path.join(__dirname, "../client/dist")));
 
 if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/dist")));
 	app.get("*", (req, res) => {
 		res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
 	});
