657ba72f8fc9e1d85556ac2f1f277d52d3f44ebc
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-04-28 14:56:10 +0800

    Fix: typo CreatePage

--- client/src/pages/CreatePage.tsx
+++ client/src/pages/CreatePage.tsx
@@ -2,7 +2,7 @@ import { ArrowLeft } from "lucide-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router";
 import toast from "react-hot-toast";
-import axios from "../lib/axios";
+import api from "../lib/axios";
 
 const CreatePage = () => {
 	const [title, setTitle] = useState("");
@@ -22,7 +22,7 @@ const CreatePage = () => {
 		}
 		setLoading(true);
 		try {
-			await axios.post("/notes", {
+			await api.post("/notes", {
 				title,
 				content,
 			});
