790f45c30abe9462b52c34bcec101c2d2677c750
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-04-28 11:21:46 +0800

    Fix: AxiosError: Network Error

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URL =
-	import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+	import.meta.env.MODE === "development"
+		? "http://localhost:5000/api"
+		: "https://think-board-ln50.onrender.com/api";
 const api = axios.create({
 	baseURL: BASE_URL,
 });
